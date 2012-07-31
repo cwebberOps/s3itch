6d69faa9b4d1708e7efc2c416cffbd090370c5cf
Christopher Webber <cwebber@ucr.edu>
2012-07-31 09:56:30 -0700

    Add campfire support

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ Bundler.setup
 require 'sinatra'
 require 'fog'
 require 'mime/types'
+require 'tinder'
 
 class S3itchApp < Sinatra::Base
 
@@ -23,6 +24,13 @@ class S3itchApp < Sinatra::Base
       content_type = MIME::Types.type_for(params[:name]).first.content_type
       file = bucket.files.create(key: params[:name], public: true, body: request.body.read, content_type: content_type)
       puts "Uploaded file #{params[:name]} to SDSC"
+      begin
+        if ENV['CAMPFIRE']
+          post_to_campfire("http://#{ENV['BUCKET']}/#{params[:name]}")
+        end
+      rescue => e
+        puts "Posting to campfire failed: #{e.message}"
+      end
       redirect "http://#{ENV['BUCKET']}/#{params[:name]}", 201
     rescue => e
       puts "Error uploading file #{params[:name]} to SDSC: #{e.message}"
@@ -43,5 +51,11 @@ class S3itchApp < Sinatra::Base
   def bucket
     sdsc = Fog::Storage.new(provider: 'Rackspace', rackspace_api_key: ENV['RACKSPACE_API_KEY'], rackspace_username: ENV['RACKSPACE_USERNAME'], rackspace_auth_url: ENV['RACKSPACE_AUTH_URL'])
     sdsc.directories.get(ENV['BUCKET'])
+  end
+
+  def post_to_campfire(:url)
+    campfire = Tinder::Campfire.new ENV['CAMPFIRE_SUBDOMAIN'], :token => ENV['CAMPFIRE_TOKEN']
+    room = campfire.find_room_by_name(ENV['CAMPFIRE_ROOM'])
+    room.speak :url
   end
 end
